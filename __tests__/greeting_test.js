b206312c6a8d97b530bbd966858f710447e19e0a
Henry Obedekah <henry.obedekah@student.vinci.be>
2023-11-12 21:28:43 +0100

    feat: greet function returns 'Hello, Jill and Jane.' when given '[Jill, Jane]'

--- __tests__/greeting_test.js
+++ __tests__/greeting_test.js
@@ -8,9 +8,15 @@ test("Test for the greet function: handle null, undefined and empty values with 
     expect(greet(null)).toBe("Hello, my friend.");
     expect(greet(undefined)).toBe("Hello, my friend.");
     expect(greet("")).toBe("Hello, my friend.");
+   
 
 })
 
 test("Test for the greet function: handle upper case", () => {
     expect(greet("JERRY")).toBe("HELLO, JERRY!");
 })
+
+test("Test for the greet function: handle array of 2 names", () => {
+    expect(greet( ["Jill", "Jane"])).toBe("Hello, Jill and Jane.");
+})
+
--- greeting.js
+++ greeting.js
@@ -1,7 +1,10 @@
 function greet(name){
     if(name === null || name ===undefined || name ==="") return "Hello, my friend.";
+
+    if(Array.isArray(name) && name.length === 2) return `Hello, ${name[0]} and ${name[1]}.`; 
     
     if(name === name.toUpperCase()) return "HELLO, "+ name +"!";
+
 
     return "Hello, "+ name +".";
 }
