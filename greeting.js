c696b6829fbd52e82c88d4caaf26542419726e30
Julien Remmery <julien.remmery@student.vinci.be>
2023-11-12 21:57:08 +0100

    feat: refactor greet function

--- greeting.js
+++ greeting.js
@@ -1,9 +1,7 @@
 function greet(name){
     if(name === null || name ===undefined || name ==="") return "Hello, my friend.";
 
-    if(Array.isArray(name) && name.length === 2) return `Hello, ${name[0]} and ${name[1]}.`;
-
-    if(Array.isArray(name) && name.length > 2){
+    if(Array.isArray(name) && name.length >= 2) {
         let out = "Hello, ";
         for(let i = 0;i < name.length; i++){
             out += name[i];
