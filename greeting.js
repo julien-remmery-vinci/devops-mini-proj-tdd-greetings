83151d8beb74b13648c400e0133e14e42ccdf355
Henry Obedekah <henry.obedekah@student.vinci.be>
2023-11-12 22:35:42 +0100

    feat: greet function returns 'Hello, Amy and Charlotte. AND HELLO BRIAN ! ' when given '[Amy, BRIAN, Charlotte]'

--- greeting.js
+++ greeting.js
@@ -1,22 +1,49 @@
+const { table } = require("console");
+
 function greet(name){
     if(name === null || name ===undefined || name ==="") return "Hello, my friend.";
 
     if(Array.isArray(name) && name.length >= 2) {
+        const sortedNames = sortArray(name); 
         let out = "Hello, ";
-        for(let i = 0;i < name.length; i++){
-            out += name[i];
-            if(i===name.length-2) {
-                out += " and "+name[i+1];
+        for(let i = 0;i < sortedNames[0].length; i++){
+            out += sortedNames[0][i];
+            if(i===sortedNames[0].length-2) {
+                out += " and "+sortedNames[0][i+1];
                 break;
             }
             else out += ", ";
         }
-        return out+=".";
+         out+=".";
+         if(sortedNames[1].length > 0 ){
+            out += " AND HELLO ";
+            for(let i = 0; i < sortedNames[1].length ;i++){
+            out += sortedNames[1][i]; 
+            
+            }
+            out+=" !"; 
+         }
+         return out ; 
+       
     }
+    
+
     
     if(name === name.toUpperCase()) return "HELLO, "+ name +"!";
 
     return "Hello, "+ name +".";
 }
+function sortArray(name){
+    const lowerCaseNames = []; 
+    const upperCaseNames = [];
+    for ( value  of name ){
+        if (value === value.toUpperCase()) upperCaseNames.push(value); 
+        else lowerCaseNames.push(value); 
+    }
+    return [lowerCaseNames,upperCaseNames];
+
+}
+
+
 
 module.exports = greet;
