b0d817d96a99f9d43f97b4a2fc9cd294f10d75dc
Julien Remmery <julien.remmery@student.vinci.be>
2023-11-12 21:07:53 +0100

    feat: refactor greet function

--- greeting.js
+++ greeting.js
@@ -1,7 +1,6 @@
 function greet(name){
-    if(name === null || name ===undefined || name ===""){
-        return "Hello, my friend.";
-    }
+    if(name === null || name ===undefined || name ==="") return "Hello, my friend.";
+    
     if(name === name.toUpperCase()) return "HELLO, "+ name +"!";
 
     return "Hello, "+ name +".";
